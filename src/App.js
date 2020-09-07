a584ab4bc999c2feca892de9d19c9944f53a3763
Ben Senescu <senescu.b@husky.neu.edu>
2020-09-07 18:26:33 -0400

    hooks for app and table

--- src/App.js
+++ src/App.js
@@ -1,58 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 import ArtistTable from './components/artist-table/artist-table';
 import SearchForm from './components/search-form/search-form';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      search_params: {
-        minFollowers: 0,
-        maxFollowers: 0,
-        minEngagementRate: 0
-      },
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-  }
+function App() {
+  const [searchParams, setSearchParams] = useState({
+    minFollowers: 0,
+    maxFollowers: 0,
+    minEngagementRate: 0
+  })
   
-  handleClick({minFollowers, maxFollowers, minEngagementRate}) {
-    const engagement_rate = (minEngagementRate / 100).toFixed(2);
+  const handleClick = ({minFollowers, maxFollowers, minEngagementRate}) => {
+    const engagementRate = (minEngagementRate / 100).toFixed(2);
     console.log('clicked')
 
-    this.setState({search_params: {
+    setSearchParams({
       minFollowers,
       maxFollowers,
-      minEngagementRate: engagement_rate
-    }});
+      minEngagementRate: engagementRate
+    });
   }
- 
-  render(){
-    const formPadding = {
-      paddingTop: '4em'
-    };
 
-    return (
-      <div className="App">
-        <div className="left-section">
-          <p className="sub-header">ARTIST FINDER</p>
-          <p className="main-title">Find musicians with your desired engagement</p>
-          <p className="sub-title">from the most popular playlists</p>
-          <div style={formPadding}>
-            <SearchForm handleClick={this.handleClick}/>
-          </div>
-          
+  return (
+    <div className="App">
+      <div className="left-section">
+        <p className="sub-header">ARTIST FINDER</p>
+        <p className="main-title">Find musicians with your desired engagement</p>
+        <p className="sub-title">from the most popular playlists</p>
+        <div style={{paddingTop: '4em'}}>
+          <SearchForm handleClick={handleClick}/>
         </div>
-        <div className="right-section">
-          <ArtistTable search_params={this.state.search_params}></ArtistTable>
-        </div>
+        
       </div>
-    );
-    
-  } 
+      <div className="right-section">
+        <ArtistTable searchParams={searchParams}></ArtistTable>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/artist-table/artist-table.js
+++ src/components/artist-table/artist-table.js
@@ -1,23 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import firebase from '../../firebase';
 
 import { Table } from 'antd';
 
-class ArtistTable extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      artists: []
-    }
-    this.updateArtists = this.updateArtists.bind(this)
-  }
-
-  updateArtists() {
-    console.log(this.props.search_params.minFollowers, this.props.search_params.maxFollowers)
+function ArtistTable(props) {
+  const [artists, setArtists] = useState([]);
+  
+  useEffect(() => {
     firebase.collection('artists')
-      .where("followers", ">", this.props.search_params.minFollowers)
-      .where("followers", "<", this.props.search_params.maxFollowers)
+      .where("followers", ">", props.searchParams.minFollowers)
+      .where("followers", "<", props.searchParams.maxFollowers)
       .limit(100)
       .get()
       .then((res) => {
@@ -27,104 +19,94 @@ class ArtistTable extends React.Component {
 
         const engagingArtists = []
         docs.forEach((doc) => {
-          if (doc.data().engagement_rate > this.props.search_params.minEngagementRate) {
+          if (doc.data().engagement_rate > props.searchParams.minEngagementRate) {
             engagingArtists.push(doc.data())
           }
         })
-        
-        const engagingArtistsFormatted = engagingArtists.map(({
-          avg_comments,
-          avg_likes,
-          engagement_rate,
-          followers,
-          following,
-          genre,
-          ig_handle,
-          media_uploads,
-          song_listens,
-          song_name,
-          soundcloud_name,
-          timestamp,
-        }) => {
-          return {
-            avg_comments,
-            avg_likes,
-            engagement_rate: (engagement_rate * 100).toFixed(2) + '%',
-            followers,
-            following,
-            genre: genre?.W_?.H ?? genre,
-            ig_handle: ig_handle?.W_?.H ?? ig_handle,
-            media_uploads,
-            song_listens,
-            song_name: song_name?.W_?.H ?? song_name,
-            soundcloud_name: soundcloud_name?.W_?.H ?? soundcloud_name,
-            timestamp: timestamp?.W_?.H,
-            engaged_fans: Math.floor(followers * engagement_rate),
-          }
-        });
 
-        this.setState({artists: engagingArtistsFormatted});
+        const engagingArtistsFormatted = formatFirebaseData(engagingArtists);
+
+        setArtists(engagingArtistsFormatted);
       })
       .catch((err) => console.error(err));
-  }
+  }, [props.searchParams])
 
-  componentDidMount() {
-    this.updateArtists()
-  }
-  
-  componentDidUpdate(prevProps) {
-    if(this.props.search_params !== prevProps.search_params) // Check if it's a new user, you can also use some unique property, like the ID  (this.props.user.id !== prevProps.user.id)
-    {
-      this.updateArtists();
-    }
-  } 
-
-  
-  render() {
-    const columns = [
-      {
-        title: 'Soundcloud Name',
-        dataIndex: 'soundcloud_name',
-      },
-      {
-        title: 'Instagram Link',
-        dataIndex: 'ig_handle',
-        render: (text, record) => (
-          <a href={`https://www.instagram.com/${record.ig_handle}`}>{record.ig_handle}</a>
-        )
-      },
-      {
-        title: 'IG Posts',
-        dataIndex: 'media_uploads',
-      },
-      {
-        title: 'Followers',
-        dataIndex: 'followers',
-        sorter: {
-          compare: (a, b) => b.followers - a.followers
-        },
-      },
-      { 
-        title: 'Engagement Rate',
-        dataIndex: 'engagement_rate',
-        sorter: {
-          compare: (a, b) => b.engagement_rate - a.engagement_rate,
-        },
-      },
-      { 
-        title: 'Engaged Fans',
-        dataIndex: 'engaged_fans',
-        sorter: {
-          compare: (a, b) => b.engaged_fans - a.engaged_fans,
-          multiple: 1,
-        },
+  const formatFirebaseData = (artists) => {
+    return artists.map(({
+      avg_comments,
+      avg_likes,
+      engagement_rate,
+      followers,
+      following,
+      genre,
+      ig_handle,
+      media_uploads,
+      song_listens,
+      song_name,
+      soundcloud_name,
+      timestamp,
+    }) => {
+      return {
+        avg_comments,
+        avg_likes,
+        engagement_rate: (engagement_rate * 100).toFixed(2) + '%',
+        followers,
+        following,
+        genre: genre?.W_?.H ?? genre,
+        ig_handle: ig_handle?.W_?.H ?? ig_handle,
+        media_uploads,
+        song_listens,
+        song_name: song_name?.W_?.H ?? song_name,
+        soundcloud_name: soundcloud_name?.W_?.H ?? soundcloud_name,
+        timestamp: timestamp?.W_?.H,
+        engaged_fans: Math.floor(followers * engagement_rate),
       }
-    ];
-  
-    return (
-      <Table dataSource={this.state.artists} columns={columns} />
-    ) 
+    });
   }
+
+  const columns = [
+    {
+      title: 'Soundcloud Name',
+      dataIndex: 'soundcloud_name',
+    },
+    {
+      title: 'Instagram Link',
+      dataIndex: 'ig_handle',
+      render: (text, record) => (
+        <a href={`https://www.instagram.com/${record.ig_handle}`}>{record.ig_handle}</a>
+      )
+    },
+    {
+      title: 'IG Posts',
+      dataIndex: 'media_uploads',
+    },
+    {
+      title: 'Followers',
+      dataIndex: 'followers',
+      sorter: {
+        compare: (a, b) => b.followers - a.followers
+      },
+    },
+    { 
+      title: 'Engagement Rate',
+      dataIndex: 'engagement_rate',
+      sorter: {
+        compare: (a, b) => b.engagement_rate - a.engagement_rate,
+      },
+    },
+    { 
+      title: 'Engaged Fans',
+      dataIndex: 'engaged_fans',
+      sorter: {
+        compare: (a, b) => b.engaged_fans - a.engaged_fans,
+        multiple: 1,
+      },
+    }
+  ];
+  
+  return (
+    <Table dataSource={artists} columns={columns} />
+  ) 
 }
 
 export default ArtistTable;
